96df2f3209589a70a3829deebca52b24733f0b66
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-10-29 20:15:08 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan63-GradienGarisLurus/src/pbo2/pkg10117070/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Aditya
+ */
+public interface GarisLurus {
+
+    public int hitungGradien();
+
+}
